019832b34a09f24ed8e9a0f9310f8968421e5a92
Forrest <forrestlaine@gmail.com>
2018-08-22 15:48:39 -0700

    updated band

--- src/scalapack/pbandsolve_lqr.c
+++ src/scalapack/pbandsolve_lqr.c
@@ -9,10 +9,12 @@
 #include <time.h>
 #include "mkl.h"
 
+/* Definition of MIN and MAX functions */
 #define MAX(a,b)((a)<(b)?(b):(a))
 #define MIN(a,b)((a)>(b)?(b):(a))
 
-typdef MKL_INT MDESC[ 9 ];
+/* Definition of matrix descriptor */
+typedef MKL_INT MDESC[ 9 ];
 
 /* Parameters */
 const double zero = 0.0E+0, one = 1.0E+0, two = 2.0E+0;
@@ -31,16 +33,16 @@ int main(int argc, char *argv[]) {
 
 /*  Matrix descriptors */
   MDESC descA, descb, descx;
+  MKL_INT descA_b[7];
+  MKL_INT descb_b[7];
 
 /*  Local scalars */
-  MKL_INT iam, nprocs, ictxt_r, ictxt_c, myrow, mycol;
+  MKL_INT iam, nprocs, ictxt_r, ictxt_c, myrow, mycol, nrows, ncols;
   MKL_INT nominal_block_size, lld;
   MKL_INT T, nx, nu, total_size, local_size;
-  MKL_INT i, j, info;
   MKL_INT half_bw, band_matrix_rows, lwork, info, ipiv;
 
-  int nx_int, nu_int, nproc_int, T_int;
-  int nrows_int, ncols_int;
+  int nx_int, nu_int, T_int;
 
 /*  Local arrays */
   double *A, *x, *b, *work;
@@ -58,7 +60,7 @@ int main(int argc, char *argv[]) {
 
 /*  Open input file */
   if ( iam == 0 ) {
-    fin = fopen( "lqr.in", "r" );
+    fin = fopen( "../in/lqr.in", "r" );
     if ( fin == NULL ) {
       printf( "Error while open input file." );
       return 2;
@@ -69,15 +71,9 @@ int main(int argc, char *argv[]) {
   if ( iam == 0 ) {
 
 /*      Read parameters */
-    fscanf( fin, "%d nx, dimension of state vectors, must be > 0 ", &n_int );
-    fscanf( fin, "%d nu, dimension of control vectors, must be > 0 ", &n_int );
-    fscanf( fin, "%d T, trajectory length, must be > 0 ", &n_int );
-
-//    fscanf( fin, "%d n, dimension of vectors, must be > 0 ", &n_int );
-//    fscanf( fin, "%d nb, size of blocks, must be > 0 ", &nb_int );
-//    fscanf( fin, "%d p, number of rows in the process grid, must be > 0", &nprow_int );
-//    fscanf( fin, "%d q, number of columns in the process grid, must be > 0, p*q = number of processes", &npcol_int );
-//    fscanf( fin, "%lf threshold for residual check (to switch off check set it < 0.0) ", &thresh );
+    fscanf( fin, "%d nx, dimension of state vectors, must be > 0 ", &nx_int );
+    fscanf( fin, "%d nu, dimension of control vectors, must be > 0 ", &nu_int );
+    fscanf( fin, "%d T, trajectory length, must be > 0 ", &T_int );
 
     T = (MKL_INT) T_int;
     nx = (MKL_INT) nx_int;
@@ -98,77 +94,100 @@ int main(int argc, char *argv[]) {
     iwork[ 2 ] = T;
     iwork[ 3 ] = total_size;
     iwork[ 4 ] = nominal_block_size;
-    igebs2d_( &ictxt, "All", " ", &i_five, &i_one, iwork, &i_five );
+    igebs2d_( &ictxt_c, "All", " ", &i_one, &i_five, iwork, &i_one );
   } else {
 
 /*      Recieve and unpack data */
-    igebr2d_( &ictxt, "All", " ", &i_five, &i_one, iwork, &i_five, &i_zero, &i_zero );
+    igebr2d_( &ictxt_c, "All", " ", &i_one, &i_five, iwork, &i_one, &i_zero, &i_zero );
     nx = iwork[ 0 ];
     nu = iwork[ 1 ];
     T = iwork[ 2 ];
     total_size = iwork[ 3 ];
     nominal_block_size = iwork[ 4 ];
   }
-  half_bw = 2*nx + nu - 1;
-  band_matrix_rows = 4*half_bw + 1;
 
+  half_bw = (MKL_INT) (2*nx + nu - 1);
+  band_matrix_rows = (MKL_INT) (4*half_bw + 1);
 
   if ( iam == 0 ) { fclose( fin ); }
-
-  blacs_gridinit_( &ictxt_r, "R", &nproc, &i_one );
-  blacs_gridinfo_( &ictxt_c, &nproc, &i_one, &myrow, &mycol );
-
+  
+  blacs_gridinfo_( &ictxt_c, &nrows, &ncols, &myrow, &mycol );
 /*  Compute precise length of local pieces and allocate array on
     each process for parts of distributed vectors */
 
   local_size = numroc_( &total_size, &nominal_block_size, &mycol, &i_zero, &nprocs );
-  lwork = (local_size+half_bw)*(2*half_bw)+6*(2*half_bw)*3*half_bw+(local_size+6*half_bw);
+  lwork = (MKL_INT) ((local_size+half_bw)*(2*half_bw)+6*(2*half_bw)*3*half_bw+(local_size+6*half_bw));
+  
+  printf(" Local size: %d\n", local_size);
+  printf(" Total size: %d\n", total_size);
+  printf(" Band matrix rows: %d\n", band_matrix_rows);
 
-  A = (double*) mkl_calloc(block_size*band_matrix_rows, sizeof( double ), 64);
-  x = (double*) mkl_calloc(block_size, sizeof( double ), 64);
-  b = (double*) mkl_calloc(block_size, sizeof( double ), 64);
+  A = (double*) mkl_calloc(local_size*band_matrix_rows, sizeof( double ), 64);
+  b = (double*) mkl_calloc(local_size, sizeof( double ), 64);
   work = (double*) mkl_calloc(lwork, sizeof( double ), 64);
 
   int k;
-  for (k = 0; k < block_size; ++k) {
-    x[k] = rand();
+  for (k = 0; k < local_size; ++k) {
     b[k] = rand();
   }
-  for (k = 0; k < block_size*band_matrix_rows; ++k) {
-    A[k] = rand();
+  int z, idx;
+  idx = 0;
+
+  for (k = 0; k < local_size; ++k) {
+  	for (z = 0; z < band_matrix_rows; ++z) {
+		if( z > 2*half_bw) {
+			A[idx] = rand();
+		} else {
+			A[idx] = 0.;
+		}
+		idx += 1;
+	}
   }
-
 /*  Initialize descriptors for distributed arrays */
-  descinit_( descA, &band_matrix_rows, &total_size, &band_matrix_rows, &nominal_block_size, &i_zero, &i_zero, &ictxt_c, &band_matrix_rows, &info );\
-  descinit_( descb, &total_size, &i_one, &nominal_block_size, &i_one, &i_zero, &i_zero, &ictxt_r, &local_size, &info );
-  descinit_( descx, &total_size, &i_one, &nominal_block_size, &i_one, &i_zero, &i_zero, &ictxt_r, &local_size, &info );
+  descA_b[0] = (MKL_INT) 501;
+  descA_b[1] = ictxt_c;
+  descA_b[2] = total_size;
+  descA_b[3] = local_size;
+  descA_b[4] = i_zero;
+  descA_b[5] = band_matrix_rows;
 
-  blacs_barrier(ictxt_c, 'A');
-
+  descb_b[0] = (MKL_INT) 502;
+  descb_b[1] = ictxt_c;
+  descb_b[2] = total_size;
+  descb_b[3] = local_size;
+  descb_b[4] = i_zero;
+  descb_b[5] = local_size;
+ 
+  printf("nominal block size: %d\n", nominal_block_size); 
+ 
+  //descinit_( descA, &band_matrix_rows, &total_size, &band_matrix_rows, &nominal_block_size, &i_zero, &i_zero, &ictxt_c, &band_matrix_rows, &info );
+  //descinit_( descb, &total_size, &i_one, &nominal_block_size, &i_one, &i_zero, &i_zero, &ictxt_c, &local_size, &info );
+  blacs_barrier_(&ictxt_c, "A");
   clock_t start = clock(), diff;
-  pdgbsv_( &total_size, &half_bw, &half_bw, &i_one, A, &i_one, descA, &ipiv, B, &i_one, descb, work, &lwork, &info );
 
-  blacs_barrier(ictxt_c, 'A');
+
+  pdgbsv_( &total_size, &half_bw, &half_bw, &i_one, A, &i_one, descA_b, &ipiv, b, &i_one, descb_b, work, &lwork, &info );
+  printf("made it out\n");
+  printf("size needed: %d\n", work[0]);
+  blacs_barrier_(&ictxt_c, "A");
+  printf("info: %d\n", info); 
   diff = clock() - start;
 
   int msec = diff * 1000 / CLOCKS_PER_SEC;
-  if (iam == 0) {
     printf("Time taken %d seconds %d milliseconds \n", msec/1000, msec%1000);
-  }
-
+ 
   mkl_free( work );
   mkl_free( A );
-  mkl_free( x );
   mkl_free( b );
-
+  printf("Herenow\n");
 /*  Destroy process grid */
-  blacs_gridexit_( &ictxt_r );
   blacs_gridexit_( &ictxt_c );
+  printf("here\n");
   blacs_exit_( &i_zero );
 
 /*  Check if residual passed or failed the threshold */
-  if ( ( iam == 0 ) && ( thresh >= zero ) && !( residual <= thresh ) ){
-    printf( "FAILED. Residual = %05.16f\n", residual );
+  if ( iam == 0 && info != 0 ){
+    printf( "FAILED. \n" );
     return 1;
   } else {
     return 0;
